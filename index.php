37685f0cda9f6cab9476e89fc768a038424224a8
tenshirelief <100312148+tenshirelief@users.noreply.github.com>
2022-02-23 13:55:56 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+
+<?php echo '<p>Hello World</p>'; ?> 
 <?php echo file_get_contents("inc/header-general.html"); ?> 
 <?php echo file_get_contents("inc/body-general.html"); ?> 
 <?php echo file_get_contents("inc/footer-general.html"); ?>
 <?php echo file_get_contents("inc/copyright.html"); ?> 
+
+ </body>
+</html>
+
