a56c2f98d837e58525d5ba05b0739fc161a23426
tenshirelief <100312148+tenshirelief@users.noreply.github.com>
2022-02-23 13:37:50 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,4 @@
-index.php
+<?php echo file_get_contents("inc/header-general.html"); ?> 
+<?php echo file_get_contents("inc/body-general.html"); ?> 
+<?php echo file_get_contents("inc/footer-general.html"); ?>
+<?php echo file_get_contents("inc/copyright.html"); ?> 
